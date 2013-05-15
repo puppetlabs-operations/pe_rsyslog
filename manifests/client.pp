e8d63563cc5eaa4c61dc7184d6aff1894792f008
Derrick Dymock <derrick@puppetlabs.com>
2013-05-15 11:06:27 -0700

    Fix incorrect name for params and remove ssl.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,10 +1,8 @@
 class pe_rsyslog::client {
 
-  include pe_puppetdb::params
+  include pe_rsyslog::params
 
   class { 'rsyslog::client':
     server  => $pe_rsyslog::params::server,
-    ssl     => $pe_rsyslog::params::ssl,
-    ssl_ca  => $pe_rsyslog::params::ssl_ca,
   }
 }
